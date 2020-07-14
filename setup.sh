fa7d13fff2e3e7fb21abca0b2b58e6b938320b09
aslikr211234 <arturo211234@gmail.com>
2020-07-14 00:22:40 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ sudo xbps-install -Syu
 sudo xbps-install -S
 sudo xbps-install git vim nano evince bash wget
 
-sudo xbps-install subversion yasm cvs cmake ragel gcc automake make
-
 cd ~/Desktop
 mkdir stuff
 cd stuff
@@ -15,13 +13,5 @@ git clone https://github.com/cbrnix/Flatery.git
 sudo mv ~/Desktop/stuff/Flatery/Flatery-Dark /usr/share/icons
 
 clear; clear;
-
-git clone git://git.videolan.org/vlc/vlc-3.0.git
-
-cd vlc-3.0/contrib/
-mkdir native
-cd native
-../bootstrap
-make
 
 echo "Now just use the Flatery Dark theme in the settings, and ur all good to go"
