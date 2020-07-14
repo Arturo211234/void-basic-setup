54333daaaf1257574b4fa7f4bd8815e026dbfa0d
aslikr211234 <arturo211234@gmail.com>
2020-07-14 03:32:39 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo xbps-install -S
+sudo xbps-install git; sudo xbps-install vim; sudo xbps-install nano; sudo xbps-install evince; sudo xbps-install bash;
+
+
+cd ~/Desktop
+mkdir stuff
+cd stuff
+
+git clone https://github.com/cbrnix/Flatery.git
+
+sudo mv ~/Desktop/stuff/Flatery/Flatery-Dark /usr/share/icons
+
+clear; clear;
+
+echo "Now just use the Flatery Dark theme in the settings, and ur all good to go"
